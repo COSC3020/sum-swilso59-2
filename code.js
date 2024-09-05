0b0bd639e369779e5b9b880305035596b80022f8
Sean W. <104464288+swilso59@users.noreply.github.com>
2024-09-05 11:50:44 -0600

    Update code.js
    Re-commit trying to see if it will update the changes made.

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function sum(a) {
-    var sum = 0;
+    var sum = 0; // Changing the value for the variable sum to 0, instead of having it set to the first value in the array. 
     for(var i = 0; i < a.length; i++) {
         sum += a[i];
     }
